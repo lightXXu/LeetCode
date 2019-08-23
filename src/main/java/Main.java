d78747b6925fd91beb4eb46719039b1e0ed825bf
lightXXu <xushenghui1989@outllokc.com>
2019-08-23 17:01:11 +0800

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+/**
+ * 功能说明：Main.java<br>
+ * 软件著作权：恒生电子股份有限公司 版权所有<br>
+ * 系统版本：v1.0<br>
+ * 开发人员： xush22487@hundsun.com<br>
+ * 开发时间：2019-08-23 17:00 <br>
+ * 功能描述：<br>
+ */
+public class Main {
+	public static void main(String[] args){
+		System.out.println("first commit");
+	}
+}
